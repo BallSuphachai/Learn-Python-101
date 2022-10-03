d23ae8f389ccbe952f5f3147fa6d880dca503899
BallSuphachai <mo055632136@gmail.com>
2022-10-03 13:11:25 +0700

    List102.py

--- AjNest/List102.py
+++ AjNest/List102.py
@@ -3,3 +3,23 @@ print(color_list[0])
 print(color_list[1])
 print(color_list[2])
 print(color_list[3])
+
+#Add an item to the list
+color_list.append("White")
+print(color_list)
+
+#Insert an item at a given position
+color_list.insert(2,"Grey") #แทรก "Grey" ไปที่ตำแหน่งที่ 2
+print(color_list)
+
+#Modify an element using the index of the he element
+color_list[2] = "Pink"
+print(color_list)
+
+#Remove an item from the list
+color_list.remove("Black")
+print(color_list)
+
+#Remove all items from the list
+color_list.clear()
+print(color_list)
