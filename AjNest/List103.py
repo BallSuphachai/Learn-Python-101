c84d002a5c0aa9d4c7b8ceba6e7d291b6c6c7c88
BallSuphachai <mo055632136@gmail.com>
2022-10-03 13:37:58 +0700

    List104

--- AjNest/List103.py
+++ AjNest/List103.py
@@ -1,2 +1,20 @@
 color_list = ["Red", "Blue", "Green", "Black"]
 print(color_list[0:2])
+print(color_list[1:2])
+print(color_list[1:-1])
+print(color_list[:3])
+print(color_list[:])
+
+col = color_list.pop(2)
+print(col)
+print(color_list)
+
+print(color_list.index("Red")) 
+
+color_list = ["Red", "Blue", "Green", "Black"]
+print(color_list)
+color_list.append("Green")
+print(color_list)
+color_list.append("Green")
+print(color_list)
+print(color_list.count("Green"))
